0d53a5a232d530247c188ce289ac35282735e553
amodkala <amodkala@gmail.com>
2023-01-01 21:02:53 -0500

    added nix flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+    description = "flake for golang + grpc development";
+
+    inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    outputs = { self, nixpkgs }:
+    let
+        system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.${system};
+    in {
+        devShell.${system} = pkgs.mkShell {
+            buildInputs = with pkgs; [
+               buf
+               go 
+            ];
+        };
+    };
+}
+
